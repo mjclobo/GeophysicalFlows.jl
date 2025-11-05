84011776f100ae24031b61604f81146aecc0b426
mjclobo <mattlobo@princeton.edu>
2025-11-05 17:40:02 -0500

    debugging dyn_nu

--- src/multilayerqg.jl
+++ src/multilayerqg.jl
@@ -349,6 +349,7 @@ function hyperviscosity(params, grid)
   L = device_array(dev){T}(undef, (grid.nkr, grid.nl, numberoflayers(params)))
   @. L = - params.ν * grid.Krsq^params.nν
   @views @. L[1, 1, :] = 0
+  x=r
 
   return L
 end
